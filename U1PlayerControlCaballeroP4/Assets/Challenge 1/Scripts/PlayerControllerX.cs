2ce524a55f8ddf08ad81b04263e9858f96b328bc
AspenTree <719391@student.dusd.net>
2025-03-14 11:43:50 -0700

    plane done

--- U1PlayerControlCaballeroP4/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ U1PlayerControlCaballeroP4/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 1f;
     public float rotationSpeed = 60;
     public float verticalInput;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +21,12 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * horizontalInput);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
